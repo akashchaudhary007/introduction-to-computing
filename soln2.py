0199854205deff07d465af69affc6cf2cd5b852f
akashchaudhary007 <104254876+akashchaudhary007@users.noreply.github.com>
2022-05-06 23:21:56 +0530

    Add files via upload

---
+++ soln2.py
@@ -0,0 +1,34 @@
+C:\Python310\python.exe C:/Users/HP/PycharmProjects/hobbit/Assignment2.py
+A) Length of the given string:  35
+
+B) Reverse of the given string is :
+egaugnal evitisnes esac a si nohtyP
+
+C) New sliced string is:
+ a case sensitive
+
+D) String after replacing:
+ Python is object oriented language
+
+E) Index of substring 'a' is: 10
+
+F) Sting without white spaces:
+ Pythonisacasesensitivelanguage
+Hey, AKASH CHAUDHARY Here!
+My SID is 21107107
+I am from Mechanical Engineering department and my CGPA is 9.9
+A. a & b = 8
+B. a | b = 58
+C. a ^ b = 50
+D. a << 2 =  224
+E. a >> 2 =  14  and  b >> 4 =  0
+Enter any string: name
+Yes
+Enter First length : 54
+Enter Second length : 78
+Enter Third length : 98
+The triangle can be formed?
+Yes!
+Enter 1st number (a) 2
+Enter 2nd number (b) 5
+Number of bits needed to be flipped to convert ‘a’ to ‘b’ are: 3
